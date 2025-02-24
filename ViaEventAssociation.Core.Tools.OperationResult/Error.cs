da6e53a9a44aab44b6f9a091af7d5942fe1aca17
Darko Goluža <darkogoluza@gmail.com>
2025-02-24 12:28:18 +0100

    Add operation result pattern

---
+++ ViaEventAssociation.Core.Tools.OperationResult/Error.cs
@@ -0,0 +1,24 @@
+﻿namespace ViaEventAssociation.Core.Tools.OperationResult;
+
+public class Error
+{
+    public int code { get; }
+    public string message { get; }
+
+    public Error(string message, int code)
+    {
+        this.code = code;
+        this.message = message;
+    }
+
+    public override string ToString() => $"[{code}]: {message}";
+    
+    // These errors can be in separate classes in the future.
+    // These are only examples of how to create different errors
+
+    // default http errors
+    public static Error NotFound() => new Error("Not found!", 404);
+
+    // User validation
+    public static Error UserDoesNotExists(int userId) => new Error($"User with id {userId} does not exist!", 404);
+}
---
+++ ViaEventAssociation.Core.Tools.OperationResult/Result.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace ViaEventAssociation.Core.Tools.OperationResult;
+
+public class Result<T>
+{
+    public List<Error> errors { get; } = new List<Error>();
+    public bool isFailure => errors.Count > 0;
+    public T payload { get; }
+
+    protected Result(T? value, List<Error> errors)
+    {
+        this.errors = errors;
+        payload = value;
+    }
+
+    public override string ToString()
+    {
+        var text = "";
+        foreach (var error in errors)
+            text += $"- {error}\n";
+
+        return text;
+    }
+
+    public static Result<T> Success(T value) => new Result<T>(value, new List<Error>());
+    public static Result<T> Failure(params Error[] errors) => new Result<T>(default, errors.ToList());
+
+    public static Result<None> Success() => Result<None>.Success(None.Value);
+
+    public static implicit operator Result<T>(T value) => Success(value);
+    public static implicit operator Result<T>(Error error) => Failure(error);
+    public static implicit operator Result<T>(Error[] errors) => Failure(errors);
+
+    public static Result<T> Combine<T>(params Result<T>[] results)
+    {
+        var failures = results.Where(r => r.isFailure).SelectMany(r => r.errors).ToList();
+
+        if (failures.Any())
+            return Result<T>.Failure(failures.ToArray());
+
+        return results.First(r => !r.isFailure);
+    }
+}
