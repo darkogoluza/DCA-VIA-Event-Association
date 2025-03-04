1f970257c18e5c3cfe2eaaa16f59ad3a2e93cae9
Darko Goluža <darkogoluza@gmail.com>
2025-03-04 13:28:02 +0100

    Add Base for domain model

---
+++ VIAEventAssociation.Core.Domain/Common/Bases/Entity.cs
@@ -0,0 +1,31 @@
+﻿namespace VIAEventAssociation.Core.Domain.Common.Bases;
+
+// This class was taken from teacher, -> https://github.com/TroelsMortensen/DomainCentricArchitectureCourse/blob/master/CodeExamples/DomainCenteredArchitectureExamples/src/Core/DCAExamples.Core.Domain/Common/Bases/Entity.cs
+
+public abstract class Entity
+{
+    public Guid Id { get; }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    protected Entity() {}
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        Entity entity = (Entity)obj;
+        return entity.Id.Equals(Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
