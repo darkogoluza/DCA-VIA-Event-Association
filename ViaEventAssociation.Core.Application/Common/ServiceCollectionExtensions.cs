7760e69742b6e2064bea08eaa4823212b4b8ddf6
Darko Goluža <darkogoluza@gmail.com>
2025-04-03 14:08:12 +0200

    Add auto-register handlers

---
+++ ViaEventAssociation.Core.Application/Common/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using ViaEventAssociation.Core.Application.Common.CommandHandler;
+
+namespace ViaEventAssociation.Core.Application.Common;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCommandHandlers(this IServiceCollection services, Assembly assembly)
+    {
+        var commandHandlerInterfaceType = typeof(ICommandHandler<>);
+
+        var handlers = assembly.GetTypes()
+            .Where(type => type.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == commandHandlerInterfaceType)
+            )
+            .ToList();
+
+        foreach (var handler in handlers)
+        {
+            var implementedInterface = handler.GetInterfaces()
+                .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == commandHandlerInterfaceType);
+
+            services.AddScoped(implementedInterface, handler);
+        }
+
+        return services;
+    } 
+}
