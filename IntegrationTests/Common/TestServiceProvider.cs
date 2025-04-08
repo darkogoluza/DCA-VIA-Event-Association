ff877643eba9315bebb67cadece75a509dc7aa07
Darko Goluža <darkogoluza@gmail.com>
2025-04-08 19:53:54 +0200

    Setup integration tests

---
+++ IntegrationTests/Common/TestServiceProvider.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ViaEventAssociation.Core.Application.Common.CommandDispatcher;
+using ViaEventAssociation.Core.Application.Common.CommandHandler;
+using ViaEventAssociation.Core.Application.Features.Event.CreateEvent;
+using VIAEventAssociation.Core.Domain.Common.Repositories;
+using ViaEventAssociation.Infrastructure.SqliteDmPersistence;
+using ViaEventAssociation.Infrastructure.SqliteDmPersistence.UnitOfWork;
+using ViaEventAssociation.Infrastructure.SqliteDmPersistence.VeaEventPersistence;
+
+namespace IntegrationTests.Common;
+
+public static class TestServiceProvider
+{
+    public static ServiceProvider CreateServiceProvider()
+    {
+        var services = new ServiceCollection();
+
+        /*
+        services.AddDbContext<SqliteDmContext>(options =>
+            options.UseSqlite(@"Data Source = C:\Users\Darko\Desktop\VIA\Semester 7\DCA\VIAEventAssociation\ViaEventAssociation.Infrastructure.SqliteDmPersistence\VEADatabaseProduction.db"));
+         * 
+         */
+        services.AddDbContext<SqliteDmContext>(options =>
+            options.UseInMemoryDatabase(databaseName: "InMemoryVeaEventDb"));
+
+        services.AddScoped<IEventRepository, VeaEventSqliteRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<ICommandHandler<CreateEventCommand>, CreateEventHandler>();
+
+        services.AddScoped<CommandDispatcher>();
+        services.AddScoped<ICommandDispatcher>(sp =>
+            new CommandSaveChanges(
+                sp.GetRequiredService<CommandDispatcher>(),
+                sp.GetRequiredService<IUnitOfWork>()
+            )
+        );
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        return serviceProvider;
+    }
+}
---
+++ IntegrationTests/Repositories/VeaEvent/CreateVeaEventTests.cs
@@ -0,0 +1,33 @@
+﻿using IntegrationTests.Common;
+using Microsoft.Extensions.DependencyInjection;
+using ViaEventAssociation.Core.Application.Common.CommandDispatcher;
+using ViaEventAssociation.Core.Application.Features.Event.CreateEvent;
+using VIAEventAssociation.Core.Domain.Common.Repositories;
+using Xunit.Abstractions;
+
+namespace IntegrationTests.Repositories.VeaEvent;
+
+public class CreateVeaEventTests
+{
+    private readonly ICommandDispatcher _commandDispatcher;
+    private readonly ServiceProvider _serviceProvider;
+
+
+    public CreateVeaEventTests(ITestOutputHelper output)
+    {
+        _serviceProvider = TestServiceProvider.CreateServiceProvider();
+
+        _commandDispatcher = _serviceProvider.GetRequiredService<ICommandDispatcher>();
+    }
+
+    [Fact]
+    public async Task CreateEventCommand_ShouldSucceedAndPersistEvent()
+    {
+        // Arrange
+        var repo = _serviceProvider.GetRequiredService<IEventRepository>();
+        var command = CreateEventCommand.Create().payload;
+
+        // Act
+        var result = await _commandDispatcher.DispatchAsync(command);
+    }
+}
