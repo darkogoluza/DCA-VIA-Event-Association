9eeac632933417aca0ec38e7731eb017a8388a5f
Darko Goluža <darkogoluza@gmail.com>
2025-04-08 14:11:22 +0200

    Implement unit of work

---
+++ ViaEventAssociation.Infrastructure.SqliteDmPersistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VIAEventAssociation.Core.Domain.Common.Repositories;
+
+namespace ViaEventAssociation.Infrastructure.SqliteDmPersistence.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DbContext _context;
+
+    public UnitOfWork(DbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
