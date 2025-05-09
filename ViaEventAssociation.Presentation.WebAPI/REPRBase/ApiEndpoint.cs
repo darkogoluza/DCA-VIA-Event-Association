fb45466d1b7ec2f91b647a45412c608008b841c6
Darko Goluža <darkogoluza@gmail.com>
2025-05-09 13:31:14 +0200

    Add REPB Base

---
+++ ViaEventAssociation.Presentation.WebAPI/REPRBase/ApiEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViaEventAssociation.Presentation.WebAPI.REPRBase;
+
+public static class ApiEndpoint
+{
+    public static class WithRequest<TResponse> 
+    {
+        public abstract class WithResponse<TResponse> : EndpointBase
+        {
+            public abstract Task<ActionResult<TResponse>> HandleAsync();
+        }
+
+        public abstract class WithoutResponse : EndpointBase
+        {
+            public abstract Task<ActionResult> HandleAsync();
+        }
+    }
+
+    public static class WithoutRequest
+    {
+        public abstract class WithResponse<TResponse> : EndpointBase
+        {
+            public abstract Task<ActionResult<TResponse>> HandleAsync();
+        }
+
+        public abstract class WithoutResponse : EndpointBase
+        {
+            public abstract Task<ActionResult> HandleAsync();
+        }
+    }
+}
+
+[ApiController]
+[Route("api")]
+public abstract class EndpointBase : ControllerBase
+{
+}
